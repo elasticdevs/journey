6ed2c6a944e880c5f1d7ff7abe46e0f9c841cfe6
journey-server <journey@server>
2023-11-09 12:29:02 +0000

    added some data on homepage

--- lib/journey_web/controllers/page_controller.ex
+++ lib/journey_web/controllers/page_controller.ex
@@ -1,9 +1,20 @@
 defmodule JourneyWeb.PageController do
+  alias Journey.Prospects
+  alias Journey.Analytics
   use JourneyWeb, :controller
 
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+
+    count_clients = length(Prospects.list_clients())
+    count_browsings = length(Analytics.list_browsings())
+    count_visits = length(Analytics.list_visits())
+
+    render(conn, :home,
+      count_clients: count_clients,
+      count_browsings: count_browsings,
+      count_visits: count_visits
+    )
   end
 end
