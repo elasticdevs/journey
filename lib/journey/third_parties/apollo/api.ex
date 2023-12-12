b393c80ec67ef3b5f8099e42064743a179201e4e
journey-server <journey@server>
2023-12-12 09:56:36 +0000

    added extract_phone()

--- lib/journey/third_parties/apollo/api.ex
+++ lib/journey/third_parties/apollo/api.ex
@@ -35,6 +35,7 @@ defmodule Journey.ThirdParties.Apollo.API do
       end
 
     organization = client["organization"]
+    client_phone = extract_phone(client)
 
     {
       %{
@@ -58,64 +59,18 @@ defmodule Journey.ThirdParties.Apollo.API do
         city: client["city"],
         state: client["state"],
         country: client["country"],
-        organization_id: client["organization"]["id"],
         job_title: client["title"],
         linkedin: linkedin,
-        phone: Enum.at(client["phone_numbers"], 0)["sanitized_number"],
+        phone: client_phone,
         seniority: client["seniority"],
         source: source
       }
     }
   end
 
-  # Not needed as of now
-  # def get_company_by_organization_id(organization_id) do
-  #   source = Application.fetch_env!(:journey, Journey.ThirdParties.Apollo.API)[:source]
-  #   url = Application.fetch_env!(:journey, Journey.ThirdParties.Apollo.API)[:companies_url]
-
-  #   body = %{
-  #     api_key: Application.fetch_env!(:journey, Journey.ThirdParties.Apollo.API)[:api_key],
-  #     organization_ids: [organization_id]
-  #   }
-
-  #   company =
-  #     case HTTPoison.post(
-  #            url,
-  #            Jason.encode!(body),
-  #            [
-  #              {"content-type", "application/json"}
-  #            ]
-  #          ) do
-  #       {:ok, %{status_code: 200, body: body}} ->
-  #         Enum.at(Jason.decode!(body)["organizations"], 0)
-
-  #       {:ok, %{status_code: 404}} ->
-  #         # do something with a 404
-  #         Logger.error("APOLLO_COMPANIES_POST_404, status_code=404")
-
-  #         []
-
-  #       {:error, %{reason: reason}} ->
-  #         # do something with an error
-  #         Logger.error("APOLLO_COMPANIES_POST_ERROR, reason=#{reason}")
-
-  #         []
-  #     end
-
-  #   %Company{
-  #     external_id: client["id"],
-  #     name: client["name"],
-  #     email: client["email"],
-  #     city: client["city"],
-  #     state: client["state"],
-  #     country: client["country"],
-  #     organization_id: client["organization"]["id"],
-  #     job_title: client["title"],
-  #     linkedin: Helpers.get_linkedin_from_linkedin_url(client["linkedin_url"]),
-  #     phone: Enum.at(client["phone_numbers"], 0)["sanitized_number"],
-  #     seniority: client["seniority"],
-  #     source: source
-  #   }
-  #   nil
-  # end
+  def extract_phone(client) do
+    phone_numbers = client["phone_numbers"] || []
+    phone_number = Enum.at(phone_numbers, 0) || %{}
+    phone_number["sanitized_number"] || nil
+  end
 end
