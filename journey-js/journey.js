096a3e07d7ffc568918ae118af1eba02e1531d1c
Shailesh Narayan <shailesh@elasticdevs.io>
2023-12-17 13:54:26 +0530

    added journey-js

---
+++ journey-js/journey.js
@@ -0,0 +1,75 @@
+let gdpr_accepted = Cookies.get("gdpr_accepted");
+if (gdpr_accepted != "true") {
+  const notice = new SimpleGDPR({
+    link: "./privacy",
+    callback: () => {
+      notice.close();
+      Cookies.set("gdpr_accepted", "true", { expires: 10000 });
+      logVisitToJourney();
+    },
+  });
+}
+
+startLogging();
+function startLogging() {
+  logVisitToJourney();
+
+  let url = location.href;
+  document.body.addEventListener(
+    "click",
+    () => {
+      setTimeout(function () {
+        if (url !== location.href) {
+          url = location.href;
+          logVisitToJourney();
+        }
+      }, 3000);
+    },
+    true
+  );
+}
+
+function logVisitToJourney() {
+  let url = new URL(document.location);
+  let params = new URLSearchParams(url.search);
+  let gdpr_accepted = "true" || Cookies.get("gdpr_accepted");
+  let client_uuid = Cookies.get("client_uuid") || params.get("uuid");
+  let browsing_uuid = Cookies.get("browsing_uuid");
+
+  let utm_campaign = params.get("utm_campaign");
+  let utm_source = params.get("utm_source");
+  let utm_medium = params.get("utm_medium");
+  let utm_term = params.get("utm_term");
+  let utm_content = params.get("utm_content");
+
+  fetch("https://elasticdevs.io/analytics/visits", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      visit: {
+        gdpr_accepted: gdpr_accepted,
+        client_uuid: client_uuid,
+        browsing_uuid: browsing_uuid,
+        page: window.location.pathname,
+        hash: window.location.hash,
+        utm_campaign: utm_campaign,
+        utm_source: utm_source,
+        utm_medium: utm_medium,
+        utm_term: utm_term,
+        utm_content: utm_content,
+      },
+    }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then((response_json) => {
+      Cookies.set("browsing_uuid", response_json.browsing_uuid, {
+        expires: 10000,
+      });
+    });
+}
