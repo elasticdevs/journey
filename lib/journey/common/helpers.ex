031c63de8428dfb8c0ece285b2a719bfb86cbdbf
journey-server <journey@server>
2023-12-30 07:51:33 +0000

    added a small dashboard on home page

--- lib/journey/common/helpers.ex
+++ lib/journey/common/helpers.ex
@@ -6,4 +6,27 @@ defmodule Journey.Common.Helpers do
   def get_linkedin_from_linkedin_id(linkedin_id) do
     "https://www.linkedin.com/in/#{linkedin_id}"
   end
+
+  def map_in_last_secs_to_string(in_last_secs) do
+    in_last_secs_maps = %{
+      "all" => "Since beginning",
+      600 => "In last 10 mins",
+      1800 => "In last 30 mins",
+      3600 => "In last 1 hour",
+      10800 => "In last 3 hours",
+      21600 => "In last 6 hours",
+      43200 => "In last 12 hours",
+      86400 => "In last 1 day",
+      172_800 => "In last 2 days",
+      259_200 => "In last 3 days",
+      432_000 => "In last 5 days",
+      604_800 => "In last 1 week",
+      2_592_000 => "In last 1 month",
+      7_776_000 => "In last 3 months",
+      15_552_000 => "In last 6 months",
+      31_536_000 => "In last 1 year"
+    }
+
+    in_last_secs_maps[in_last_secs] || nil
+  end
 end
--- lib/journey_web/controllers/page_controller.ex
+++ lib/journey_web/controllers/page_controller.ex
@@ -1,4 +1,6 @@
 defmodule JourneyWeb.PageController do
+  alias Journey.Common.Helpers
+  alias Journey.Activities
   alias Journey.Prospects
   alias Journey.Analytics
   use JourneyWeb, :controller
@@ -15,16 +17,32 @@ defmodule JourneyWeb.PageController do
 
   def home(conn, _params) do
     in_last_secs = get_in_last_secs_from_cookie(conn)
+    in_last_secs_string = Helpers.map_in_last_secs_to_string(in_last_secs)
+
+    companies = Prospects.list_companies()
+
+    clients_all = Prospects.list_clients(conn.assigns.current_user, %{in_last_secs: "all"})
+    visits_all = Analytics.list_visits(conn.assigns.current_user, %{in_last_secs: "all"})
+    activities_all = Activities.list_activities(conn.assigns.current_user, %{in_last_secs: "all"})
 
     clients = Prospects.list_clients(conn.assigns.current_user, %{in_last_secs: in_last_secs})
     browsings = Analytics.list_browsings(conn.assigns.current_user, %{in_last_secs: in_last_secs})
     visits = Analytics.list_visits(conn.assigns.current_user, %{in_last_secs: in_last_secs})
 
+    activities =
+      Activities.list_activities(conn.assigns.current_user, %{in_last_secs: in_last_secs})
+
     conn
     |> render(:home,
+      companies: companies,
+      clients_all: clients_all,
+      visits_all: visits_all,
+      activities_all: activities_all,
       clients: clients,
       browsings: browsings,
-      visits: visits
+      visits: visits,
+      activities: activities,
+      in_last_secs_string: in_last_secs_string
     )
   end
 end
